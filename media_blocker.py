ce2992ae5f646f911ed9f9d4faeafb1d5c70b78f
Shuwei Chen <shuwei@flexport.com>
2020-05-13 08:05:51 -0700

    create blocker class

--- media_blocker.py
+++ media_blocker.py
@@ -3,49 +3,52 @@ from datetime import datetime as dt
 import os
 import fileinput
 
-websites=["www.youtube.com", "www.facebook.com", "www.reddit.com", "www.linkedin.com"]
+class Blocker:
+    def __init__(self):
+        # starting with an initial set of sites that the user can modify
+        self.blocked_sites = ["www.youtube.com", "www.facebook.com", "www.reddit.com", "www.linkedin.com"]
 
-def check_block(status):
-    # block
-    if status == True:
-        with open('/etc/hosts', 'rt') as original_host_file:
-            copy_host_file = original_host_file.read()
-            # print(copy_host_file)
+    def current_sites(self):
+        print("Current sites include:")
+        print(self.blocked_sites)
+
+    def check_block(self, status):
+        # block
+        if status == True:
+            with open('/etc/hosts', 'rt') as original_host_file:
+                copy_host_file = original_host_file.read()
+                with open('/tmp/etc_hosts.tmp', 'a+') as outf:
+                    outf.write(copy_host_file)
+                    for site in self.blocked_sites:
+                        # print(site)
+                        if site in copy_host_file:
+                        # print(site)
+                            pass
+                        else:
+                            print(site)
+                            outf.write('\n' + '127.0.0.1' + " " + site)
+
+                    os.system('sudo mv /tmp/etc_hosts.tmp /etc/hosts')
+        # unblock
+        else:
+            with open('/etc/hosts', 'rt') as original_host_file:
+                copy_host_file = original_host_file.read()
+
+            lines = copy_host_file.splitlines()
+
             with open('/tmp/etc_hosts.tmp', 'a+') as outf:
-                outf.write(copy_host_file)
-                for site in websites:
-                    # print(site)
-                    if site in copy_host_file:
-                    # print(site)
-                        pass
+                for line in lines:
+                    if not any(site in line for site in self.blocked_sites):
+                        if line != '':
+                            outf.write(line + '\n')
                     else:
-                        print(site)
-                        outf.write('\n' + '127.0.0.1' + " " + site)
-                        # print(outf.read())
+                        # print(line)
+                        pass
+                outf.close()
 
-                os.system('sudo mv /tmp/etc_hosts.tmp /etc/hosts')
-    # unblock
-    else:
-        with open('/etc/hosts', 'rt') as original_host_file:
-            copy_host_file = original_host_file.read()
+            os.system('sudo mv /tmp/etc_hosts.tmp /etc/hosts')
 
-            # print(copy_host_file)
+new_blocker = Blocker()
 
-        lines = copy_host_file.splitlines()
-
-        with open('/tmp/etc_hosts.tmp', 'a+') as outf:
-            for line in lines:
-                if not any(site in line for site in websites):
-                    if line != '':
-                        outf.write(line + '\n')
-                else:
-                    # print(line)
-                    pass
-            outf.close()
-
-        os.system('sudo mv /tmp/etc_hosts.tmp /etc/hosts')
-
-# check_block(False)
-# check_block(True)
-# test_file = open('/etc/hosts', 'rt')
-# print(test_file.read())
+# new_blocker.current_sites()
+# new_blocker.check_block(False)
