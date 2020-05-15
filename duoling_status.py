acc2cb2d541fb158731224d7282b64882bc5b043
Shuwei Chen <shuwei@flexport.com>
2020-05-14 19:42:10 -0700

    adding duolingo goal checker

---
+++ duoling_status.py
@@ -0,0 +1,34 @@
+import duolingo
+import time
+from datetime import datetime
+
+# log in
+lingo = duolingo.Duolingo('[your username here]', '[your password here]')
+
+# set your daily goal
+daily_goal = 50
+
+def check_goal_reached(duolingo, goal):
+
+    xp_json = duolingo.get_daily_xp_progress()
+    lessons_blob = xp_json['lessons_today']
+
+    lesson_xp = 0
+    current_date = datetime.today().date()
+
+    # duolingo's api endpoints return xp & lessons strangely
+    # xp & lessons carry over from previous days until a new
+    # lesson is completed, thus the below date validation
+
+    for lesson in lessons_blob:
+        lesson_localized_date = datetime.fromtimestamp(lesson['time']).date()
+
+        if lesson_localized_date == current_date:
+            lesson_xp += lesson['xp']
+
+    if lesson_xp > goal:
+        return True
+    else:
+        return False
+
+# print(check_goal_reached(lingo, daily_goal))
